8a1cc8f1925c407f17decbb6b58a8d112d64e035
Aameer-Khan <khanaameer1998@gmail.com>
2026-08-18 23:40:29 +0530

    Fix formatting and add newline at end of app.py

--- app/app.py
+++ app/app.py
@@ -15,6 +15,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+
 def get_connection():
     return psycopg2.connect(
         host=DB_HOST,
@@ -71,7 +72,6 @@ def shorten():
 
 @app.route("/<shortcode>")
 def redirect_url(shortcode):
-
     conn = get_connection()
     cur = conn.cursor()
 
